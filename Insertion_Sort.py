20ceed1f34a962fa4481a321de4efb78a0b9f4df
Aswad Sheeraz <aswadsheeraz2000@gmail.com>
2025-12-03 04:18:17 +0500

    Better Commenting

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -1,25 +1,20 @@
 Number = [9,3,5,2,4,1,12]
 
 def InsertionSort():
+    # Insertion sort builds a sorted prefix one element at a time (ascending).
     Array_size = len(Number)
 
-    for pointer in range(1, Array_size):
-        # Making a temp variable which is to be inserted at correct position
-        Value_To_Insert = Number[pointer]
-        # Making Hole_Position (Correct position that we are trying to find)
-        Hole_Poisition = pointer 
+    for pointer in range(1, Array_size):  # pointer marks the first unsorted element
+        Value_To_Insert = Number[pointer]  # value we need to insert into the sorted prefix
+        Hole_Position = pointer            # current hole index where insertion may land
 
-        # Conditional Loop for Shifting and finding correct position
+        # Move left while items are larger than the value we want to insert
+        while Hole_Position > 0 and Number[Hole_Position - 1] > Value_To_Insert:
+            Number[Hole_Position] = Number[Hole_Position - 1]  # shift larger item right
+            Hole_Position = Hole_Position - 1                  # hole slides left
 
-        while Hole_Poisition > 0 and Number[Hole_Poisition - 1] > Value_To_Insert:
-
-            # Shifting
-            # Hole Position Decrement
-
-            Number[Hole_Poisition] = Number[Hole_Poisition - 1]
-            Hole_Poisition = Hole_Poisition - 1
-        
-        Number[Hole_Poisition] = Value_To_Insert
+        # Drop the saved value into its correct position
+        Number[Hole_Position] = Value_To_Insert
 
 InsertionSort()
 print(Number)
